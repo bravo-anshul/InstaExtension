d55f3ec319a3fc83d11569caf1ca41317daa639f
bravo-anshul <52098161+bravo-anshul@users.noreply.github.com>
2022-05-20 02:04:37 +0530

    scrolling whole div.

--- content.js
+++ content.js
@@ -5,28 +5,18 @@ function gotMessage (message, sender, sendResponse){
         elt.innerHTML=message.txt;
     }
     let seenByButton = document.getElementsByClassName('eVFA4');
-    seenByButton[0].click();
+    //seenByButton[0].click();
     let userLists = document.getElementsByClassName('_7UhW9   xLCgt      MMzan    _0PwGv              fDxYl     ');
-    // for(user of userLists){
-    //     console.log(user.innerHTML);
-    // }
-    let lastUsername = '';
-    let usernames = [];
-    while(lastUsername != userLists[userLists.length-1]){
-        console.log('before');
-        wait(3000);  //7 seconds in milliseconds
-        console.log('after');
-        userLists[userLists.length-1].scrollIntoView();
-        userLists = document.getElementsByClassName('_7UhW9   xLCgt      MMzan    _0PwGv              fDxYl     ');
-        for(user of userLists){
-            console.log(user.innerHTML);
-        }
-        console.log('before');
-        wait(3000);  //7 seconds in milliseconds
-        console.log('after');
-        lastUsername = userLists[userLists.length-1];
-
+    for(user of userLists){
+        console.log(user.innerHTML);
     }
+    let lastUsername = '';
+    let divBox = document.getElementsByClassName('             qF0y9          Igw0E     IwRSH      eGOV_       acqo5  vwCYk                                                                                lDRO1                               ');
+    
+    let child = divBox[0].children;
+    child[0].scrollTop = child[0].scrollHeight;
+    
+    
     
     
 }
