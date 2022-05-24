e9c67561ce12de4ea9ae148e652aeb27778da91c
bravo-anshul <52098161+bravo-anshul@users.noreply.github.com>
2022-05-24 22:56:43 +0530

    Manually creating user list

--- content.js
+++ content.js
@@ -1,24 +1,75 @@
 chrome.runtime.onMessage.addListener(gotMessage);
+var lastUsernmaeRecieved;
+var usernameList = [];
 function gotMessage (message, sender, sendResponse){
-    let paragraphs=document.getElementsByClassName('eebAO h_uhZ ');
-    for (elt of paragraphs){
-        elt.innerHTML=message.txt;
+    if(message == "scrollTop"){
+        console.log("scrolling up");
+        scrollDivUp();
     }
-    let seenByButton = document.getElementsByClassName('eVFA4');
-    //seenByButton[0].click();
-    let userLists = document.getElementsByClassName('_7UhW9   xLCgt      MMzan    _0PwGv              fDxYl     ');
-    for(user of userLists){
-        console.log(user.innerHTML);
+    else if(message == "makeList"){
+        console.log("Make list called");
+        makeList();
     }
-    let lastUsername = '';
+    else{
+        console.log("username recieved from popup"+ message);
+        lastUsernmaeRecieved = message;
+        let paragraphs=document.getElementsByClassName('eebAO h_uhZ ');
+        for (elt of paragraphs){
+            elt.innerHTML=message.txt;
+        }
+        let seenByButton = document.getElementsByClassName('eVFA4');
+        //seenByButton[0].click();
+        //printUsers();
+        scrollDiv();
+        // chrome.runtime.sendMessage("got message from content script", function(response){
+        //     console.log(response.farewell);
+        // });
+    }
+}
+
+function makeList(){
     let divBox = document.getElementsByClassName('             qF0y9          Igw0E     IwRSH      eGOV_       acqo5  vwCYk                                                                                lDRO1                               ');
-    
+    let child = divBox[0].children;
+    console.log("child Box height"+ (child[0].scrollHeight));
+    for(let x = 0; x<(child[0].scrollHeight);x+=10){
+        console.log("For loop scrolling");
+        child[0].scrollTop = x;   
+        printUsers(); 
+    }
+
+}
+
+function scrollDivUp(){
+    let divBox = document.getElementsByClassName('             qF0y9          Igw0E     IwRSH      eGOV_       acqo5  vwCYk                                                                                lDRO1                               ');
+    let child = divBox[0].children;
+    child[0].scrollTop = -(child[0].scrollHeight);
+}
+
+function scrollDiv(){
+    let divBox = document.getElementsByClassName('             qF0y9          Igw0E     IwRSH      eGOV_       acqo5  vwCYk                                                                                lDRO1                               ');
     let child = divBox[0].children;
     child[0].scrollTop = child[0].scrollHeight;
+}
+
+function printUsers(){
     
-    
-    
-    
+    let userLists = document.getElementsByClassName('_7UhW9   xLCgt      MMzan    _0PwGv              fDxYl     ');
+    for(user of userLists){
+        addUserInList(user.innerHTML);
+        //console.log(user.innerHTML);
+        user.scrollIntoView();
+    }
+    console.log(usernameList);
+
+    // if(lastUsernmaeRecieved != userLists[userLists.length - 1].innerHTML )
+    //     chrome.runtime.sendMessage(userLists[userLists.length - 1].innerHTML);
+    // else
+    //     lastUsernmaeRecieved = userLists[userLists.length - 1].innerHTML;
+}
+
+function addUserInList(user){
+    if(!usernameList.includes(user))
+        usernameList.push(user);
 }
 
 function wait(ms){
--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 
 document.getElementById("myButton").addEventListener("click", myFunction);
+document.getElementById("myTopButton").addEventListener("click", myfunctionNew);
+document.getElementById("makeList").addEventListener("click", makeList);
+
+var lastusername = '';
+var count = 0;
+
+function myfunctionNew(){
+  let params={
+    active: true,
+    currentWindow: true
+  }
+  chrome.tabs.query(params, gotTabs);
+  function gotTabs(tabs){
+    console.log("lastUsername is pop up script :"+lastusername)
+    //for(let x =0;x<5;x++){
+      chrome.tabs.sendMessage(tabs[0].id, "scrollTop");
+    //   wait(4000);
+    // }
+}
+}
+
+
+function makeList(){
+  let params={
+    active: true,
+    currentWindow: true
+  }
+  chrome.tabs.query(params, gotTabs);
+  function gotTabs(tabs){
+    console.log("lastUsername is pop up script :"+lastusername)
+    //for(let x =0;x<5;x++){
+      chrome.tabs.sendMessage(tabs[0].id, "makeList");
+    //   wait(4000);
+    // }
+  } 
+}
 
 function myFunction() {
     let params={
@@ -8,10 +44,42 @@ function myFunction() {
       }
       chrome.tabs.query(params, gotTabs);
       function gotTabs(tabs){
-          console.log("asfasf");
-        let msg={
-          txt: "hello"
-        }
-        chrome.tabs.sendMessage(tabs[0].id, msg);
+        console.log("lastUsername is pop up script :"+lastusername)
+        //for(let x =0;x<5;x++){
+          chrome.tabs.sendMessage(tabs[0].id, lastusername);
+        //   wait(4000);
+        // }
     }
+}
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse){
+  
+  if(lastusername != message){
+    myFunction();
+    lastusername = message;
+    console.log("last username is "+ message);
+    console.log(" count is "+ count);
+
+    count+=1;
+  }
+  else if (count < 1000){
+    myFunction();
+    lastusername = message;
+    count+=1;
+
+  }
+    
+
+  console.log(message);
+}
+
+
+function wait(ms){
+  var start = new Date().getTime();
+  var end = start;
+  while(end < start + ms) {
+    end = new Date().getTime();
+ }
 }
